4fdb1fc8ab7b7c2d68d083e04efa9f889e091cd5
sco1 <sco1.git@gmail.com>
2018-02-07 15:31:28 -0500

    Add time entry short circuit, --help descriptions
    Remove non-virtualenv entry point

--- worktimecalc.py
+++ worktimecalc.py
@@ -3,16 +3,18 @@ import datetime as dt
 import click
 
 @click.command()
-@click.option('--interactive' ,'-i', is_flag=True)
-@click.option('--starttime', '-s', default='0700')
-@click.option('--endtime', '-e', default='1530')
-@click.option('--workday', '-wd', default=8.5)
-def cli(starttime, endtime, interactive, workday):
+@click.option('--interactive' ,'-i', is_flag=True, help='Enable interactive mode')
+@click.option('--starttime', '-s', default='0700', help="Start time, 'HHMM'")
+@click.option('--endtime', '-e', default='1530', help="End time, 'HHMM'")
+@click.option('--workday', '-wd', default=8.5, help='Length of workday, decimal hours')
+@click.option('--timetuple', '-tt', nargs=3, type=(str, str, float), help="Short circuit tuple, 'HHMM' 'HHMM' x.x")
+def cli(starttime, endtime, interactive, workday, timetuple):
     """
     Calculate leave/premium time based on start and end times.
-
-    Time is input as HHMM
     """
+    if timetuple:
+        starttime, endtime, workday = timetuple
+
     if interactive:
         starttime = click.prompt('Enter Start Time', default=starttime)
         endtime = click.prompt('Enter End Time', default=endtime)
@@ -36,8 +38,3 @@ def cli(starttime, endtime, interactive, workday):
         click.echo('You worked exactly ', nl=False)
         click.secho('one ', nl=False, fg='green')
         click.echo('full workday!')
-
-
-# Add an entry point for use outside of a venv
-if __name__ == '__main__':
-    cli()
