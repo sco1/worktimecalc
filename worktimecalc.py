84bc45ce34fa72e579d9fd8d5e3b7d72841882e6
sco1 <sco1.git@gmail.com>
2018-02-07 15:17:32 -0500

    Add preliminary command line options
    Add options for start/end times, workday length
    Add flag for interactive mode

--- worktimecalc.py
+++ worktimecalc.py
@@ -3,15 +3,23 @@ import datetime as dt
 import click
 
 @click.command()
-def cli():
+@click.option('--interactive' ,'-i', is_flag=True)
+@click.option('--starttime', '-s', default='0700')
+@click.option('--endtime', '-e', default='1530')
+@click.option('--workday', '-wd', default=8.5)
+def cli(starttime, endtime, interactive, workday):
     """
     Calculate leave/premium time based on start and end times.
 
     Time is input as HHMM
     """
-    workday = 8.5  # Workday length, in hours
-    starttime = dt.datetime.strptime(click.prompt('Enter Start Time', default='0700'), '%H%M')
-    endtime = dt.datetime.strptime(click.prompt('Enter End Time', default='1530'), '%H%M')
+    if interactive:
+        starttime = click.prompt('Enter Start Time', default=starttime)
+        endtime = click.prompt('Enter End Time', default=endtime)
+        workday = click.prompt('Enter workday length (decimal hours)', default=workday)
+
+    starttime = dt.datetime.strptime(starttime, '%H%M')
+    endtime = dt.datetime.strptime(endtime, '%H%M')
 
     tdelta = endtime - starttime
     decimalhours = (tdelta.total_seconds() / 3600) - workday
@@ -22,12 +30,14 @@ def cli():
         click.echo('hours of leave')
     elif decimalhours > 0:
         click.echo('Request ', nl=False)
-        click.secho(f'{abs(decimalhours)} ', nl=False, fg='green')
+        click.secho(f'{decimalhours} ', nl=False, fg='green')
         click.echo('premium hours')
     else:
         click.echo('You worked exactly ', nl=False)
         click.secho('one ', nl=False, fg='green')
         click.echo('full workday!')
 
+
+# Add an entry point for use outside of a venv
 if __name__ == '__main__':
     cli()
